357dd2442585540e698234440ac70f37af9c0dea
zaptech\jun.zhu <jun.zhu@zaptechnology.com>
2017-05-10 10:26:38 +1000

    Added GitInfo for versioning.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Zap Technology Pty Ltd")]
+[assembly: AssemblyCopyright("Copyright © Zap Technology Pty. Ltd. 2017")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion(ThisAssembly.Git.SemVer.Major
+    + "." + ThisAssembly.Git.SemVer.Minor
+    + "." + ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyInformationalVersion(ThisAssembly.Git.SemVer.Major
+    + "." + ThisAssembly.Git.SemVer.Minor
+    + "." + ThisAssembly.Git.SemVer.Patch
+    + "+" + ThisAssembly.Git.Commit)]
